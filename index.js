f90121acec5584055b903f1128e80fb902dbda25
omaimaa777 <omaimaa321@gmail.com>
2024-11-19 13:44:27 +0100

    deklarerat första funktionen och for-if loop

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 // Recommended: All functions declared here
 
+function isCityFound (target) {
+    let cityFound = false;
+    let chosenCity = null;
+    const targetLC = target.toLowerCase ();
 
+    for (let cityValue of cities) {
+        const cityValueLC = cityValue.name.toLowerCase ()
+        console.log("Innan if funktionen.");
+
+        if (targetLC == cityValueLC) {
+            console.log("Inuti if-satsen")
+            //Jämför target med indexerad (chosenCity) array i Cities
+            cityFound = true;
+            h2.textContent = target;
+            chosenCity = cityValue;
+            console.log(cityFound);
+            break;
+        } else {
+            console.log("Denna stad finns ej");
+            console.log(cityFound);
+        }
+        console.log("Chosen city = '" + chosenCity + "'" );
+        console.log("______")
+
+    }
+};
 
 // Recommended: constants with references to existing HTML-elements
 
