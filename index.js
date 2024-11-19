7da100ef290f484164370cb2f6ad9e18e0686f37
omaimaa777 <omaimaa321@gmail.com>
2024-11-19 13:43:49 +0100

    länkat css filerna till html

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Recommended: All functions declared here
 
+
+
 // Recommended: constants with references to existing HTML-elements
 
+
+
 // Recommended: Ask for the city name and then the rest of the code
+
 
