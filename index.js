fb698f720f46f920d32cf4bc28066f3f48298467
omaimaa777 <omaimaa321@gmail.com>
2024-11-25 20:05:50 +0100

    tog bort föregående funktioner + hämtade referenser till html-element

--- index.js
+++ index.js
@@ -7,45 +7,6 @@ function findCityByName (name) {
 }
 
 
-function markCityBox (cityObject, kindOfCity) {
-    const cityBoxes = document.querySelectorAll(".cityBox");
-    for (let cityBox of cityBoxes) {
-        if (cityBox.textContent = cityObject.name) {
-            cityBox.classList.add("target");
-        } else if (cityBox > 1) {
-            cityBox.classList.add("closest");
-        } else {
-            cityBox.classList.add("furthest");
-        }
-    }
-}
-
-function isCityFound (target) {
-    let cityFound = false;
-    let chosenCity = null;
-    const targetLC = target.toLowerCase ();
-// sätter "chosenCity" till True/False om "target" matchar något namn i "cities"
-    for (let cityValue of cities) {
-        const cityValueLC = cityValue.name.toLowerCase ()
-        titleElem = document.querySelector("title");
-        if (targetLC == cityValueLC) {
-            //jämför target med indexderad (chosenCity) array i Cities
-            cityFound = true;
-            h2, textContent = target + "(" + cityValue.country + ")";
-            chosenCity = cityValue;
-            //sätt rätt title (fliken)
-            titleElem.innerText = cityValue.name;
-            //avbryt loopen, chosenCity=True, staden finns i databasen
-            break;
-        } else {
-            h2.textContent = target + "finns inte i databasen"
-            //sätt rätt title (fliken)
-            titleElem.innerText = "Not Found"
-        }
-        console.log("___")
-    }
-    return cityFound;
-};
 
 
 
@@ -55,11 +16,10 @@ function isCityFound (target) {
 
 // Recommended: constants with references to existing HTML-elements
 
-const h2 = document.querySelector("h2");
-const h3 = document.querySelector("h3");
-const closestCity = document.getElementById("closest");
-const furthestCity = document.getElementById("furthest");
-const divCities = document.getElementById("cities");
+const cititesDiv = document.getElementById ("cities");
+const tableDiv = document.getElementById("table");
+const h2 = document.querySelector ("h2");
+const h3 = document.querySelector ("h3");
 
 // Recommended: Ask for the city name and then the rest of the code
 
